40bf57c4ead1312e8c9d5117dea1d5bedd771edb
Sukhnoor23 <91556662+Sukhnoor23@users.noreply.github.com>
2021-10-20 22:20:32 -0700

    Lab 6

---
+++ lab4/js/lab.js
@@ -0,0 +1,17 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+//Creating an web page:
+  //First use an html to help create your website.
+      //Then find domain name and host like github.
+      Pick your website address. Domain name example; yourname.com
+      //Then start designing using stylesheets.
+       Add content through html and design a theme through lab.css.
+    //Then view in the browser and
+    inspect to make sure everything is looking how you want it.
+   //Then you commit and p
+   push to the origin to publish it.
---
+++ lab6/js/lab.js
@@ -0,0 +1,16 @@
+/* Lab 6: Arrays and Objects
+ * Author: Sukhnoor Sahota and Luke Pilapil
+ * Created: 10/20/21
+ */
+
+// Define Variables
+   make = "honda";
+   model = "element";
+   color = "silver";
+   year = "2007";
+
+// Output
+   document.writeln("honda" +  "<br>");
+   document.writeln("element" + "<br>");
+   document.writeln("silver" + "<br>");
+   document.writeln("2007" + "<br>");
