1341582b9656cea6c53b492f6fc46d8b6d79142b
Sukhnoor23 <91556662+Sukhnoor23@users.noreply.github.com>
2021-11-02 09:21:45 -0700

    Update lab9.js

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -4,14 +4,26 @@
  */
 
 
-var outputEL = document.createElement("content");
+// Use getElementById() to find your output <div> and assign it to a variable outputEl
+var outputEl = document.getElementById("output");
+console.log("outputEl: ", outputEl);
 
 // I created a new element, assigned a variable, and changed the attribute.
 var new1El = document.createElement("p");
-new1El.setAttribute('html','Hello World');
-outputEL.appendChild(new1El);
+
+// Change the html attribute of new1El
+new1El.innerHTML = "Hello World!";
 
 // Then I created a second element and repeated what I did for the first element but changed the attribute.
-var new2El =document.createElement("p");
-new2El.setAttribute('html','Goodbye World');
-outputEL.appendChild(new2El);
+var new2El = document.createElement("p");
+
+// Change the html attribute of new2El
+new2El.innerHTML = "Goodbye World!";
+
+//Append both elements <div>
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
+
+// Change the css attributes
+new1El.style.color = "pink";
+new2El.style.color = "green";
