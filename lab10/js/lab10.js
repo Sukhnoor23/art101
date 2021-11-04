7e16546e9afbec9b9d426e261b7ebe174e2d73cf
Sukhnoor23 <91556662+Sukhnoor23@users.noreply.github.com>
2021-11-03 19:28:19 -0700

    Lab 10

---
+++ lab10/js/lab10.js
@@ -0,0 +1,15 @@
+/* Lab 9: Javascript with the Web
+ * Author: Sukhnoor Sahota & Luke Pilapil
+ * Created: 11/3/21
+ */
+
+// Create an event handler for a button click.
+var button = document.getElementById('my-button');
+//Add a function to the click
+window.onload = function sayMyname() {
+  var name = prompt('What is your name');
+  var h1 = document.querySelector('h1');
+  h1.innerText = "Hello,"  +  name;
+}
+//Add an event handler to say the name.
+button.addEventListener('click', sayMyname);
