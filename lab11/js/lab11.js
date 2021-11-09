b7390141250418ec6fd7df61ab6e9c6d43479714
Sukhnoor23 <91556662+Sukhnoor23@users.noreply.github.com>
2021-11-08 19:57:23 -0800

    Lab 11

---
+++ lab11/js/lab11.js
@@ -0,0 +1,27 @@
+/* Lab 9: Javascript with the Web
+ * Author: Sukhnoor Sahota & Luke Pilapil
+ * Created: 11/3/21
+ */
+
+//using jQuery to add buttons and to make it say something.
+ $(document).on('click', '#btnClick', function(){
+     alert("Button is clicked");
+ });
+
+//changed what the button said
+ $(document).on('click', '#btnClick2', function(){
+     alert("How are you doing?");
+ });
+
+//changed the button again
+ $(document).on('click', '#btnClick3', function(){
+     alert("The Results");
+ });
+
+// Using jQuery we added a click event
+// We used the effect of a Animation
+ $(document).ready(function(){
+   $("button").click(function(){
+     $("div").animate({left: '950px'});
+   });
+ });
