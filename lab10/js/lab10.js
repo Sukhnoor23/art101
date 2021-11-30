45f61b6d7a935102f15c5ff2ecf22ded5035ef01
Sukhnoor23 <91556662+Sukhnoor23@users.noreply.github.com>
2021-11-29 21:55:00 -0800

    lab 10

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -3,13 +3,15 @@
  * Created: 11/3/21
  */
 
-// Create an event handler for a button click.
-var button = document.getElementById('my-button');
-//Add a function to the click
-window.onload = function sayMyname() {
-  var name = prompt('What is your name');
-  var h1 = document.querySelector('h1');
-  h1.innerText = "Hello,"  +  name;
-}
-//Add an event handler to say the name.
-button.addEventListener('click', sayMyname);
+ // Create an event handler for a button click.
+ var button = document.getElementById('my-button');
+
+ //Add a function to the button.
+ window.onload = function sayMyname() {
+   var name = prompt('What is your name');
+   var h1 = document.querySelector('h1');
+   h1.innerText = "Hello,"  +  name;
+ }
+
+ //Add an event handler to say the name.
+ button.addEventListener('click', sayMyname);
